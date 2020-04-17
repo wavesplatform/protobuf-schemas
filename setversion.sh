944fefd8cd96ca200e0ef8fd93970f50010e82c3
Karasiq <yoba123@yandex.ru>
2020-04-17 17:06:51 +0300

    Set version script

--- setversion.sh
+++ setversion.sh
@@ -2,9 +2,8 @@
 version_regex='v([0-9]+)\.([0-9]+)\.?([0-9]*)-([0-9]+)-g([0-9|a-z]+)'
 version_msg_regex='([0-9]+)\.([0-9]+)\.?([0-9]*)'
 git_string=$(git describe --tags --long)
-commit_message=$1
 
-if [[ $commit_message =~ $version_msg_regex ]]; then
+if [[ $1 =~ $version_msg_regex ]]; then
   major_version="${BASH_REMATCH[1]}"
   minor_version="${BASH_REMATCH[2]}"
   patch_version="${BASH_REMATCH[3]}"
@@ -13,7 +12,28 @@ elif [[ $git_string =~ $version_regex ]]; then
   major_version="${BASH_REMATCH[1]}"
   minor_version="${BASH_REMATCH[2]}"
   patch_version="${BASH_REMATCH[3]}"
-  version_string=$major_version.$minor_version.$((patch_version + 1))-SNAPSHOT
+  postfix="-SNAPSHOT"
+
+  if [[ $1 == "minor" ]]; then
+    minor_version=$((minor_version + 1))
+    patch_version=0
+    shift
+  elif [[ $1 == "major" ]]; then
+    major_version=$((major_version + 1))
+    minor_version=0
+    patch_version=0
+    shift
+  elif [[ $1 == "reset" ]]; then
+    postfix=""
+  else
+    patch_version=$((patch_version + 1))
+  fi
+
+   if [[ $1 == "release" ]]; then
+    postfix=""
+    shift
+  fi
+  version_string=$major_version.$minor_version.$patch_version$postfix
 else
   echo "Error: git describe did not output a valid version string. Unable to update version" >&2
   exit 1
